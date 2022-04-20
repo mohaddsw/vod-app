8e21439ad47a2ca7ac1593b979612974010a0af6
mohaddsw <mhds.rahimi@gmail.com>
2022-04-20 11:24:43 +0430

    config of commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,6 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["add", "wip", "feat", "fix", "change", "hotfix", "remove", "update", "refactor"]],
+  },
+};
